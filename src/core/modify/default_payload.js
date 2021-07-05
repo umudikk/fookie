a5d453795c26eaa958f2dc3b214e6fa6719d48bd
umudikk <seyitumutbicer@gmail.com>
2021-07-05 18:52:08 +0300

    dev

--- src/core/modify/default_payload.js
+++ src/core/modify/default_payload.js
@@ -1,6 +1,7 @@
 module.exports = async function (payload, ctx) {
    payload = ctx.lodash.merge(payload, {
       options: {
+         method:"get",//todo hata sysapıyor mu kontrol et
          simplified: false,
          deep: false,
       },
